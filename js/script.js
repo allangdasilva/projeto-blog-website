a925905970a9539d4d9407a31428a72e21513ca0
Allan Silva <allandasilva33@outlook.com>
2024-10-17 00:31:58 -0300

    Estilização da página principal 90% finalizada (mobile, tablet, desktop)

--- js/script.js
+++ js/script.js
@@ -1,97 +1,117 @@
+"use strict";
+
 // Menu
-let navContainer = document.querySelector('.nav-container-02')
-let navContent = document.querySelector('.nav-content-container')
-let menuClose = document.querySelector('.menu-close-content')
-let menuClick = document.querySelector('.menu-click')
+const NAV_CONTAINER_02 = document.querySelector('.nav-container-02');
+const NAV_CONTENT = document.querySelector('.nav-content-container');
+const MENU_CLOSE = document.querySelector('.menu-close-content');
+const MENU_CLICK = document.querySelector('.menu-click');
 
 
-menuClick.addEventListener('click', openMenu)
-menuClose.addEventListener('click', closeMenu)
+MENU_CLICK.addEventListener('click', openMenu);
+MENU_CLOSE.addEventListener('click', closeMenu);
 
 function openMenu(){
-    navContainer.classList.add('nav-container-02-active')
-    navContent.classList.add('nav-content-container-active')
-    menuClose.classList.add('menu-close-content-active')
+    NAV_CONTAINER_02.classList.add('nav-container-02-active');
+    NAV_CONTENT.classList.add('nav-content-container-active');
+    MENU_CLOSE.classList.add('menu-close-content-active');
 }
 function closeMenu(){
-    navContainer.classList.remove('nav-container-02-active')
-    navContent.classList.remove('nav-content-container-active')
-    menuClose.classList.remove('menu-close-content-active')
+    NAV_CONTAINER_02.classList.remove('nav-container-02-active');
+    NAV_CONTENT.classList.remove('nav-content-container-active');
+    MENU_CLOSE.classList.remove('menu-close-content-active');
 }
 
 // Slider
-let sliderIndex = 1
-showSlider(sliderIndex)
+let sliderIndex = 1;
+showSlider(sliderIndex);
 
 function nextPrev(n){
-    showSlider(sliderIndex += n)
+    showSlider(sliderIndex += n);
 }
 
 function currentSlider(n){
-    showSlider(sliderIndex = n)
+    showSlider(sliderIndex = n);
 }
 
 function sliderMouseEnter(n){
-    showSlider(sliderIndex = n)
+    showSlider(sliderIndex = n);
 }
 
 function showSlider(n){
-    let sliderImages = [...document.querySelectorAll('.slider-images-content')]
-    let titlesContainer = [...document.querySelectorAll('.titles-container')]
-    let circles = [...document.querySelectorAll('.circle')]
-    let textPerson = [...document.querySelectorAll('.titles-text-person-container')]
-    let position = [...document.querySelectorAll('.position')]
+    const SLIDER_IMAGES = [...document.querySelectorAll('.slider-images-content')];
+    const TITLES_CONTAINER = [...document.querySelectorAll('.titles-container')];
+    const CIRCLES = [...document.querySelectorAll('.circle')];
+    const TEXT_PERSON = [...document.querySelectorAll('.titles-text-person-container')];
+    const POSITION = [...document.querySelectorAll('.position')];
 
-    if(n > sliderImages.length){
-        sliderIndex = 1
+    if(n > SLIDER_IMAGES.length){
+        sliderIndex = 1;
     }
     if(n < 1){
-        sliderIndex = sliderImages.length
+        sliderIndex = SLIDER_IMAGES.length;
     }
 
-    sliderImages.map((ele)=>{
-        ele.style.display = 'none' 
+    SLIDER_IMAGES.map((ele)=>{
+        ele.style.display = 'none' ;
     })
-    titlesContainer.map((ele)=>{
-        ele.style.display = 'none'
+    TITLES_CONTAINER.map((ele)=>{
+        ele.style.display = 'none';
     })
-    circles.map((ele)=>{
-        ele.classList.remove('circle-active')
+    CIRCLES.map((ele)=>{
+        ele.classList.remove('circle-active');
     })
-    textPerson.map((ele)=>{
-        ele.classList.remove('titles-text-person-container-active')
+    TEXT_PERSON.map((ele)=>{
+        ele.classList.remove('titles-text-person-container-active');
     })
-    position.map((ele)=>{
-        ele.classList.remove('position-active')
+    POSITION.map((ele)=>{
+        ele.classList.remove('position-active');
     })
 
-    sliderImages[sliderIndex - 1].style.display = 'block'
-    titlesContainer[sliderIndex - 1].style.display = 'flex'
-    circles[sliderIndex - 1].classList.add('circle-active')
-    textPerson[sliderIndex - 1].classList.add('titles-text-person-container-active')
-    position[sliderIndex - 1].classList.add('position-active')
+    SLIDER_IMAGES[sliderIndex - 1].style.display = 'block';
+    TITLES_CONTAINER[sliderIndex - 1].style.display = 'flex';
+    CIRCLES[sliderIndex - 1].classList.add('circle-active');
+    TEXT_PERSON[sliderIndex - 1].classList.add('titles-text-person-container-active');
+    POSITION[sliderIndex - 1].classList.add('position-active');
 }
 
 // Mini Slider
-showMiniSlider(sliderIndex)
+showMiniSlider(sliderIndex);
 
 function nextPrevMiniSlider(n){
-    showMiniSlider(sliderIndex += n)
+    showMiniSlider(sliderIndex += n);
 }
 
 function showMiniSlider(n){
-    let miniSliderImages = [...document.querySelectorAll('.mini-slider-images-content')]
+    const MINI_SLIDER_IMAGES = [...document.querySelectorAll('.mini-slider-images-content')];
 
-    if(n > miniSliderImages.length){
-        sliderIndex = 1
+    if(n > MINI_SLIDER_IMAGES.length){
+        sliderIndex = 1;
     }
     if(n < 1){
-        sliderIndex = miniSliderImages.length
+        sliderIndex = MINI_SLIDER_IMAGES.length;
     }
 
-    miniSliderImages.map((ele)=>{
-        ele.style.display = 'none'
+    MINI_SLIDER_IMAGES.map((ele)=>{
+        ele.style.display = 'none';
     })
 
-    miniSliderImages[sliderIndex - 1].style.display = 'block'
+    MINI_SLIDER_IMAGES[sliderIndex - 1].style.display = 'block';
 }
+
+// Menu Fixed
+window.addEventListener('scroll', ()=>{
+    const HEADER = document.querySelector('header');
+    const MAIN = document.querySelector('main');
+    const NAV_CONTAINER_01 = document.querySelector('.nav-container-01');
+    const NAV_CONTAINER_02 = document.querySelector('.nav-container-02');
+
+    if(window.scrollY > 30 && NAV_CONTAINER_02.classList == 'nav-container-02') {
+        HEADER.classList.add('header-active');
+        MAIN.classList.add('main-active');
+        NAV_CONTAINER_01.style.paddingBlock = '10px';
+    }else{
+        HEADER.classList.remove('header-active');
+        MAIN.classList.remove('main-active');
+        NAV_CONTAINER_01.style.paddingBlock = '20px';
+    }
+})
